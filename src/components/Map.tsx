f835f31877107336dac6a75671ff25697671b5b6
AliDeWeb <alimoradi0business@gmail.com>
2025-01-16 15:21:48 +0330

    feat: add dynamic map view adjustment and popup for current location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 // components/Map.tsx
 import React from "react";
 import { Icon } from "leaflet";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMap, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 interface MapProps {
@@ -10,6 +10,18 @@ interface MapProps {
     y: number;
   };
 }
+
+const ChangeMapView: React.FC<MapProps> = ({ location }) => {
+  const map = useMap();
+
+  React.useEffect(() => {
+    if (location.x !== 0 && location.y !== 0) {
+      map.setView([location.x, location.y], 13);
+    }
+  }, [location, map]);
+
+  return null;
+};
 
 const Map: React.FC<MapProps> = ({ location }) => {
   const customIcon = new Icon({
@@ -27,7 +39,12 @@ const Map: React.FC<MapProps> = ({ location }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[location.x, location.y]} icon={customIcon} />
+      <Marker position={[location.x, location.y]} icon={customIcon}>
+        <Popup>
+          <h2>You are here ;)</h2>
+        </Popup>
+      </Marker>
+      <ChangeMapView location={location} />
     </MapContainer>
   );
 };
